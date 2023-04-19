0cd931fa7ab8af804b2aa8ab1a10257f8f4310a4
OnkabetseSedumedi <olsedumedi12@gmail.com>
2023-04-19 15:51:24 +0200

    IWA3 CHALLENGES

---
+++ IWA3/challenge1.js
@@ -0,0 +1,14 @@
+// configuration.js
+
+const company = 'ACME Inc.'
+const year = 2022
+ 
+
+// scripts.js
+
+import {
+    companyform ('configuration');}
+import year= form ('configuration')
+
+const message = '© ' + company + ' (' + year + ')'
+document.querySelector('footer').innerText = message
