2dca1e3010e7b47d51d9d0921926a53ad44d356d
OnkabetseSedumedi <olsedumedi12@gmail.com>
2023-04-24 10:44:29 +0200

    Update challenge1.js

---
+++ IWA5/challenge1.js
@@ -0,0 +1,45 @@
+FREE_WARNING = 'Free shipping only applies to single customer orders'
+BANNED_WARNING = 'Unfortunately we do not ship to your country of residence'
+NONE_SELECTED = '0'
+
+if (location = RSA) { shipping === 400 && currency === 'R' }
+
+
+shipping = 600 
+shipping = 800
+
+let shoes = 300 * 1
+let toys = 100 * 5
+let shirts = 150 * 'NONE_SELECTED'
+let batteries = 35 * 2
+let pens = 5 * 'NONE_SELECTED'
+
+
+let cart= shoes + batteries + pens + shirts
+console.log(cart)
+shipping = null
+currency = 'R'
+
+
+
+if (country = 'RSA' && cart>=1000 && customers === 1) { shipping=0 }
+
+if (customers>1) 
+{console.log(WARNING)}
+
+if (country= 'RSA'&& 1000>cart) { 
+    shipping =400
+    
+}
+
+if (country = 'RSA' & 1000>=cart) { 
+    shipping= 400
+    
+}
+
+
+location = 'NK' ? console.log(WARNING) : console.log('price', currency, shoes + batteries + pens + shirts + shipping)
+
+customers = '1'
+const location = 'RSA'
+currency = null
