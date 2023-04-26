6e7a3ef52e756c19edfe8e027773a3271cfc503a
OnkabetseSedumedi <olsedumedi12@gmail.com>
2023-04-26 07:23:15 +0200

    challenge7

--- IWA7/challenge3.js
+++ IWA7/challenge3.js
@@ -11,10 +11,18 @@ const divider = '----------------------------------'
 // Only change below this line
 
 
-const owed = parseInt('R' + leoBalance + sarahBalance)
-const leo = "{leoName} + {leoSurname} + \"Owed\" + \"R\" + {sarahBalance}"
-const sarah = "{leoName} + {surname} + \"Owed\" + \"R\" + {sarahBalance}"
-const total = "Total amount owed: "
-const result = leo + sarah + divider + divider + total + owed + divider
+let owed = parseFloat(leoBalance) + parseFloat(sarahBalance)              //used parsefloat to show the decimals and to convert to number
+owed= (owed*1).toFixed          //owed was a negative value
+owed1=owed.split('2').join(between)            //the number was not split
+owed2=owed.split('13').join(between)
+owed= owed1 + `${owed2}`            // joined the two to create the space
+
+
+const leo = `${` ${leoName} + ${leoSurname} + (Owed: R ${-1*parsefloat(leoBalance).toFixed(2)})` }`
+const sarah = `${` ${sarahName} + ${sarahSurname} + (Owed: R ${-1*parseFloat(sarahBalance).toFixed(2)})`}`
+const total = `${` ${divider} ${'Total amount owed: R'} ${owed} ${divider} `}`
+const result = `${leo} ${sarah}
+
+${total}`
 
 console.log(result)
