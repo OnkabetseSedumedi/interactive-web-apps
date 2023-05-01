038ac4a6934f8d77ca82fff84590551fa1124ebb
OnkabetseSedumedi <olsedumedi12@gmail.com>
2023-05-01 16:44:47 +0200

    Update challenge1.js

--- IWA5/challenge1.js
+++ IWA5/challenge1.js
@@ -1,67 +1,41 @@
-WARNING = 'Free shipping only applies to single customer orders'
-BANNED_WARNING = 'Unfortunately we do not ship to your country of residence'
-NONE_SELECTED = '0'
+const FREE_WARNING = 'Free shipping only applies to single customer orders'
+const BANNED_WARNING = 'Unfortunately we do not ship to your country of residence'
+const NONE_SELECTED = '0'
 
 
-let customer = 2
-let country = 'null'
-let currency = 'null'
-let shipping = 0
-
-
-shipping = 600 
-shipping = 800
+let location = 'RSA'
+let customers = '1'
+let currency = null
+let shipping = null
 
 let shoes = 300 * 1
 let toys = 100 * 5
-let shirts = 150 * 'NONE_SELECTED'
+let shirts = 150 * NONE_SELECTED
 let batteries = 35 * 2
-let pens = 5 * 'NONE_SELECTED'
+let pens = 5 * NONE_SELECTED
 
 
-let cart= shoes + batteries + pens + shirts + toys
-console.log(cart)
-country = 'B' 
-currency = 'R'
-
-
-
-if (country = 'RSA' && cart>=1000 && customer === 1) { shipping=0 }
-
-if (customer>1) {
-console.log(WARNING)}
-
-if (country= 'RSA'&& 1000>cart) { 
-    shipping =400
-    
+if (location === 'RSA') {
+    currency = 'R'
+    shipping = 400
+  } else if (location === 'NAM') {
+    currency = '$'
+    shipping = 600
+  } else if (location === 'NK') {
+    shipping = null
+    currency = null
+    console.log(BANNED_WARNING);
+  } else {
+    currency = '$'
+    shipping = 800
+  }
+const totalPrice = toys + shoes + batteries + pens + shirts
+const totalCost = totalPrice + shipping
+if (totalPrice >= 1000 && (location === 'RSA' || location === 'NAM')) {  //added the value of 1000
+        if(customers === 1) {
+            shipping = 0
+        } else {
+            console.log(FREE_WARNING)
+        }
 }
-
-if (country = 'RSA' && 1000>=cart) { 
-    shipping= 400
-    
-}
-
-if (country = 'RSA' && 1000>=cart) { 
-    shipping= 400
-    
-}
-
-if (country==='B' && cart>=60 && customer===1) {
-    shipping=0
-    
-}
-
-if (country==='B' && cart>=60) {
-    shipping=600
-}
-if (country==='B' && cart>=60 && customer>1) {
-    shipping=600
-}
-
-if (country!== 'RSA' && country!=='B') { 
-    shipping=800
-    
-}
-
-country !== 'NK' ? console.log('price', currency, shoes +toys + batteries + pens + shirts + shipping):console.log(BANNED_WARNING)
-
+console.log('price', currency,totalCost)
