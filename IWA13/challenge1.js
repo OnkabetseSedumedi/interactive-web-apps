89adda0dbbdea2ca00496d9c7420dc446ae79f04
OnkabetseSedumedi <olsedumedi12@gmail.com>
2023-05-02 19:47:08 +0200

    Update challenge1.js

--- IWA13/challenge1.js
+++ IWA13/challenge1.js
@@ -7,16 +7,20 @@ let calculated = '1'
 const logCalc = () => {
   const isString = typeof calculated === 'string' //removed numerical
   const calculatedAsNumber = isString ? parseInt(calculated) : calculated
-  calculated = calculatedAsNumber + 1
+  calculated = calculatedAsNumber + 2 //changed from 1 to 2,they used === instead of =
+  return calculated; // had no return
 }
+
+
 const calcUser = () => { //
   logCalc()
-  if (calculated > 2) user = 'John'
-  if (calculated > 2) state = 'requesting'
-  if (calculated > 3) state = 'idle'
+  if (calculated >= 2) user = 'John'
+  if (calculated >= 2) state = 'requesting'
+  if (calculated >= 3) state = 'idle'
+  return calculated;
 }
 const checkUser = () => {
-  if (user && state === 'requesting') {
+  if (user && state !== 'requesting') {
     console.log(`User: ${user} (${calculated})`)
   }
 }
