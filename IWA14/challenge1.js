0c4416544ae1ff71e7e687a9f12f2cc02f9db749
OnkabetseSedumedi <olsedumedi12@gmail.com>
2023-05-03 14:00:11 +0200

    IWA14

---
+++ IWA14/challenge1.js
@@ -0,0 +1,11 @@
+const firstName = 'John';
+const age = 35;
+const hobby = 'Coding';
+
+const logTwice = () => {
+    return (`Hello, ${firstName} (${age}). I love ${hobby}!`)
+
+}
+
+ console.log(logTwice())
+ console.log(logTwice())
---
+++ IWA14/challenge2.js
@@ -0,0 +1,46 @@
+// script.js
+
+function add(a, b) {
+     return a + b;
+     }
+
+function multiply(a, b) { 
+    return a * b; }                                           //had - instead of *
+
+function internal(a, b) {
+	result1= add(a, b)*multiply(a, b)
+	return result1
+}
+
+const result2=internal(2,4);
+console.log(result2)
+
+const result3=internal(2,2);
+console.log(result3)
+
+// Not allowed to change below this
+
+const example1 = {
+	internal: {
+		a: 2,
+		b: 4,
+		c: 8,
+	},
+	add,
+	multiply,
+  calculate: internal
+}
+
+const example2 = {
+	internal: {
+		a: 2,
+		b: 2,
+		c: 3,
+	},
+	add,
+	multiply,
+  calculate: internal
+}
+
+example1.calculate()
+example2.calculate()
